832d9f1075c2fd87fa3a47d0aa69f7ae1250793a
oshela abimiku <44838481+oshela@users.noreply.github.com>
2023-10-20 08:59:16 +0100

    Create stack-algorithm.js

---
+++ stack-algorithm.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+function TaskList() {
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState('');
+
+  const addTask = () => {
+    if (taskInput.trim() !== '') {
+      setTasks([...tasks, taskInput]);
+      setTaskInput('');
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <h1>Task List</h1>
+      <input
+        type="text"
+        placeholder="Enter a task"
+        value={taskInput}
+        onChange={(e) => setTaskInput(e.target.value)}
+      />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => removeTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
